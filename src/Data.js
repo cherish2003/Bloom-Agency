76509fc82d3167b4e81922bcffeece4fd106b1cd
cherish2003 <saicherissh90@gmail.com>
2023-05-26 21:06:07 +0530

    img srcs changes

--- src/Data.js
+++ src/Data.js
@@ -1,41 +1,47 @@
 const data = [
   {
-    avatar: "./src/assets/user2.jpg",
+    avatar:
+      "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=800",
     name: "John Anderson",
     review:
       "They delivered exceptional work with attention to detail and visually appealing designs, all within the agreed-upon timeframe. Their professionalism and clear communication made the collaboration seamless. I highly recommend Bloom Agency for exceptional UI/UX design.",
     profession: "Stellar Solutions(CEO)",
   },
   {
-    avatar: "./src/assets/user1.jpg",
+    avatar:
+      "https://images.pexels.com/photos/5384445/pexels-photo-5384445.jpeg?auto=compress&cs=tinysrgb&w=800",
     name: "David Smith",
     review:
       "I am extremely satisfied with Bloom Agency's web and mobile app development services. They maintained professionalism, clear communication, and promptly addressed any concerns or feedback. I highly recommend their reliable, high-quality, and expertly crafted applications to anyone in need of exceptional development work",
     profession: "Vertex Ventures (Coreteam)",
   },
   {
-    avatar: "./src/assets/user4.jpg",
+    avatar:
+      "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=800",
     name: "Michael Brown",
     review:
       "Bloom Agency's Digital Marketing Management services delightfully benefit businesses with expertise and strategic approaches. Their tailored strategies and optimized campaigns enhance brand visibility and engagement. Highly recommended for professional and effective digital marketing through data-driven decisions and open communication.",
     profession: "Global Innovations ",
   },
   {
-    avatar: "./src/assets/user5.jpg",
+    avatar:
+      "https://images.pexels.com/photos/2726111/pexels-photo-2726111.jpeg?auto=compress&cs=tinysrgb&w=800",
     name: "Sarah Johnson",
     review:
       "Bloom Agency's Custom WordPress Design & Development services surpass expectations with outstanding expertise and attention to detail. They create visually appealing and user-friendly websites, transforming visions into reality. Their professionalism, prompt communication, and technical expertise ensure a seamless process. Highly recommended for exceptional Custom WordPress Design & Development services.",
     profession: "Elite Enterprises ",
   },
   {
-    avatar: "./src/assets/user6.jpg",
+    avatar:
+      "https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg?auto=compress&cs=tinysrgb&w=800",
     name: "Emma Thompson",
     review:
       "Bloom Agency's Branding Design services were mind-blowing! Their rock star team created a logo and packaging that elevated my brand to new heights. The essence of my business was perfectly captured, leaving customers in awe. Thrilled with the results, I highly recommend Bloom Agency's expertise and attention to detail.",
     profession: "QuantumVision Labs ",
   },
   {
-    avatar: "./src/assets/user3.jpg",
+    avatar:
+      "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=800g",
     name: "Robert Davis",
     review:
       "Bloom Agency has been a game-changer for our company! Their expertise in app and web development has significantly enhanced our products, making them more user-friendly and efficient. The team's dedication, creativity, and attention to detail have exceeded our expectations. We highly recommend Bloom Agency for anyone seeking top-notch development services",
